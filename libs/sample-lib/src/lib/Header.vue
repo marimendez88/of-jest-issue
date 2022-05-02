45a6fb00a13c4211d4f398b3f0adf320b9c673f8
Maria Jose Mendez <majomendez88mena@gmail.com>
2022-05-02 11:38:37 -0600

    feat(cleaning): cleaning repo

--- libs/sample-lib/src/lib/Header.vue
+++ libs/sample-lib/src/lib/Header.vue
@@ -1,11 +1,22 @@
 <template>
-  <h1>This is a test header component</h1>
+<ion-page>
+  <ion-header>
+    <ion-title>
+<h1>This is a test header component</h1>
+    </ion-title>
+  </ion-header>
+</ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import {IonPage, IonHeader, IonTitle} from '@ionic/vue';
 export default defineComponent({
   name: 'Header',
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+  },
 });
 </script>
