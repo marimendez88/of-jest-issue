8c650229604af165b56b2a749350ea51514e5de9
Maria Jose Mendez <majomendez88mena@gmail.com>
2022-05-02 10:10:45 -0600

    feat(sample-app): creating sample app

---
+++ apps/sample-app/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'sample-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+.vue$': 'vue3-jest',
+    '.+.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+  coverageDirectory: '../../coverage/apps/sample-app',
+  snapshotSerializers: ['jest-serializer-vue'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'apps/sample-app/tsconfig.spec.json',
+      babelConfig: 'apps/sample-app/babel.config.js',
+    },
+    'vue-jest': {
+      tsConfig: 'apps/sample-app/tsconfig.spec.json',
+      babelConfig: 'apps/sample-app/babel.config.js',
+    },
+  },
+};
