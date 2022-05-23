9c1bfefa2ba46f46fb85729d0efbb3c6ee439721
Maria Jose Mendez <majomendez88mena@gmail.com>
2022-05-23 11:02:32 -0600

    fix(test): test

--- apps/sample-app/jest.config.js
+++ apps/sample-app/jest.config.js
@@ -1,23 +1,17 @@
 module.exports = {
   displayName: 'sample-app',
   preset: '../../jest.preset.js',
-  transform: {
-    '^.+.vue$': 'vue3-jest',
-    '.+.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
-    '^.+.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
-  coverageDirectory: '../../coverage/apps/sample-app',
-  snapshotSerializers: ['jest-serializer-vue'],
   globals: {
     'ts-jest': {
-      tsconfig: 'apps/sample-app/tsconfig.spec.json',
-      babelConfig: 'apps/sample-app/babel.config.js',
-    },
-    'vue-jest': {
-      tsConfig: 'apps/sample-app/tsconfig.spec.json',
-      babelConfig: 'apps/sample-app/babel.config.js',
+      tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+.vue$': 'vue3-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!@ionic/core|@stencil/core|ionicons)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/sample-app'
 };
